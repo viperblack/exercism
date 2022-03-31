94c7f0947410ebab16cbf03d99f227c38a98201f
viperblackcl <vimerson.silva@gmail.com>
2022-03-31 10:48:08 -0300

    First Commit

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -3,13 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
+const getWords = (phrase) => {
+  return phrase.toLowerCase().match(/\w+('\w+)?/g);
+};
+
 export const countWords = (phrase) => {
+  const dict = {};
 
-let words, count = {};
+  for (const word of getWords(phrase)) {
+    if (Object.prototype.hasOwnProperty.call(dict, word)) {
+      dict[word] += 1;
+    } else {
+      dict[word] = 1;
+    }
+  }
 
-words = phrase.split(/[ ,]+/);
-
-console.log(words, count);
-
-  
+  return dict;
 };
