af9697fa57362aced956da4b8893556f2c0924c3
viperblackcl <vimerson.silva@gmail.com>
2022-06-30 12:23:30 -0300

    First Commit

---
+++ elixir/lasagna/lib/lasagna.ex
@@ -0,0 +1,22 @@
+defmodule Lasagna do
+
+  def expected_minutes_in_oven()do
+    40
+  end
+
+  def remaining_minutes_in_oven(time)do
+    expected_minutes_in_oven() - time
+  end
+
+  def preparation_time_in_minutes(layers)do
+    layers * 2
+  end
+
+  def total_time_in_minutes(layers, time)do
+    preparation_time_in_minutes(layers) + time
+  end
+
+  def alarm()do
+    "Ding!"
+  end
+end
