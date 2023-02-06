2f10823c8fcffef3e13067055c2bdb3f37d1d4d2
viperblack <vimerson.silva@gmail.com>
2023-02-06 11:26:40 -0300

    First Commit

---
+++ java/gigasecond/src/main/java/Gigasecond.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.management.modelmbean.ModelMBean;
+
+public class Gigasecond {
+    private LocalDateTime momentPass;
+    private int GIGASECOND = 1_000_000_000;
+
+    public Gigasecond(LocalDate moment) {
+        momentPass = moment.atStartOfDay(); //atStartOfDay() method of LocalDate class in Java is combines this date with the time of midnight to create a LocalDateTime at the start of this date
+    }
+
+    public Gigasecond(LocalDateTime moment) {
+       momentPass = moment; 
+    }
+
+    public LocalDateTime getDateTime() {
+       return momentPass.plusSeconds(GIGASECOND);
+    }
+}
